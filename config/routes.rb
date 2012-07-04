c52c78a7d5904b74eefc187630b8e00550dcd825
boo1ean <boo1ean0807@gmail.com>
2012-07-04 16:33:42 +0300

    Add social controller, feed link to header

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Aggregate::Application.routes.draw do
+  get "feed" => "social#index", :as => "feed"
+
   devise_for :users, :controllers => { :registrations => "registrations", :sessions => "sessions" }
 
   root :to => "index#index"
