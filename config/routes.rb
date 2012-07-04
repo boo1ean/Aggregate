0f3061a12028ead8a0a8e7043b7009a20264ad20
boo1ean <boo1ean0807@gmail.com>
2012-07-04 11:32:56 +0300

    Add devise registrations and sessions controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Aggregate::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations", :sessions => "sessions" }
   root :to => "index#index"
 end
