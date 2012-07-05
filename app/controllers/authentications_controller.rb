b718ddf784865f7619b20dcd19b171217d201a29
boo1ean <boo1ean0807@gmail.com>
2012-07-05 21:49:34 +0300

    Add delete link for auth

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,9 +1,15 @@
 class AuthenticationsController < ApplicationController
+  before_filter :authenticate_user!
+
   def create
-    if user_signed_in?
-      omniauth = request.env["omniauth.auth"]
-      current_user.add_auth omniauth
-      redirect_to :root
-    end
+    omniauth = request.env["omniauth.auth"]
+    current_user.add_auth omniauth
+    redirect_to :root
+  end
+
+  # Delete auth
+  def destroy
+    Authentication.where(:user_id => current_user.id).destroy(params[:id])
+    redirect_to :root
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Aggregate::Application.routes.draw do
-  get "feed" => "social#index", :as => "feed"
+  get    "feed"     => "social#index", :as => "feed"
+  delete "auth/:id" => "authentications#destroy", :as => "destroy_auth"
 
   devise_for :users, :controllers => { :registrations => "registrations", :sessions => "sessions" }
 
